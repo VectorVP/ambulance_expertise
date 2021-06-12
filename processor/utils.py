0e448b5dae6b7d964f861050f9b24a12a5d9bd3f
VectorVP <vvtumko@mail.ru>
2021-06-12 23:55:21 +0300

    Add function to find code block (interval)

--- processor/utils.py
+++ processor/utils.py
@@ -1,5 +1,7 @@
 import re
 import cv2
+import icd10
+
 import pytesseract
 from pytesseract import Output
 
@@ -16,11 +18,32 @@ def parse_image(image_path):
     Returns:
         list: list of all icd-10 codes in image
     """
+
     img = cv2.imread(image_path)
     img_data = pytesseract.image_to_string(img, output_type=Output.DICT)
     pattern = r'[QDEGKLMPOZJ]\d+\.?\d+?'
-    icd_code = re.findall(pattern, img_data['text'])
-    return icd_code
+    icd_codes = re.findall(pattern, img_data['text'])
+    return icd_codes
+
+def find_icd_block(icd_codes):
+    """
+    Find hierarchy block for each icd code.
+
+    Args:
+        icd_codes (list of strings): list of icd codes to process
+
+    Returns:
+        dict: {'icd code': 'icd block'}
+    """
+
+    icd_dict = {}
+    for i in icd_codes:
+        code = icd10.find(i)
+        if code.billable:
+            icd_dict[i] = code.block
+    return icd_dict
+
+
 
 def phrase_detect(list_base, phrase):
     """
@@ -33,14 +56,19 @@ def phrase_detect(list_base, phrase):
     Returns:
         list of tuples: list of pairs word-confidence
     """
+
     list_match = process.extract(phrase, list_base, scorer=fuzz.token_set_ratio, limit=30)
-    return list_match[0][0]
+    return list_match[0]
 
 def test():
     list_base = ['привет', 'как дела', 'пойдем']
     phrase = 'делы'
-    print('phrase_detect result', phrase_detect(list_base, phrase))
-    print('parse_image result', parse_image('/home/vladislav/Downloads/photo_2021-06-10_09-49-26.jpg'))
+    phrase_detect_result = phrase_detect(list_base, phrase)
+    parse_image_result = parse_image('/home/vladislav/Downloads/photo_2021-06-10_09-49-26.jpg')
+    find_icd_block_result = find_icd_block(parse_image_result)
+    print('phrase_detect result:', phrase_detect_result)
+    print('parse_image result:', parse_image_result)
+    print('code_block result:', find_icd_block_result)
 
 if __name__ == "__main__":
     test()
