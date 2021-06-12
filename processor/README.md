c1768f67f57e7367f824e4fd906348233e0fb6c4
VectorVP <vvtumko@mail.ru>
2021-06-12 23:04:23 +0300

    Add OCR function with Tesseract and and descriptions to functions

---
+++ processor/README.md
@@ -0,0 +1,6 @@
+### Notes
+
+To use Tesseract it is necessary to install additional ppa.
+```
+sudo apt-get install tesseract-ocr
+```
--- processor/utils.py
+++ processor/utils.py
@@ -1,15 +1,46 @@
+import re
+import cv2
+import pytesseract
+from pytesseract import Output
+
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-def phrase_detect (list_base, phrase):
+def parse_image(image_path):
+    """
+    Find ICD-10 codes in the image.
+
+    Args:
+        image_path (string): image path to process
+
+    Returns:
+        list: list of all icd-10 codes in image
+    """
+    img = cv2.imread(image_path)
+    img_data = pytesseract.image_to_string(img, output_type=Output.DICT)
+    pattern = r'[QDEGKLMPOZJ]\d+\.?\d+?'
+    icd_code = re.findall(pattern, img_data['text'])
+    return icd_code
+
+def phrase_detect(list_base, phrase):
+    """
+    Find similarity between source and parsed texts.
+
+    Args:
+        list_base (list of strings): list of source text phrases
+        phrase (list of strings): list of parsed strings
+
+    Returns:
+        list of tuples: list of pairs word-confidence
+    """
     list_match = process.extract(phrase, list_base, scorer=fuzz.token_set_ratio, limit=30)
     return list_match[0]
-
 
 def test():
     list_base = ['привет', 'как дела', 'пойдем']
     phrase = 'делы'
-    print(phrase_detect(list_base, phrase))
+    print('phrase_detect result', phrase_detect(list_base, phrase))
+    print('parse_image result', parse_image('/home/vladislav/Downloads/photo_2021-06-10_09-49-26.jpg'))
 
 if __name__ == "__main__":
     test()
